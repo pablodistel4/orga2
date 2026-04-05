1fa3d353c942bc998b8f70937a44234b0ae8e48a
Pablo Distel <pablodistel4@gmail.com>
2026-04-04 22:06:35 -0300

    intro. casting --correcion

--- casting/casting.c
+++ casting/casting.c
@@ -7,7 +7,7 @@ int main() {
    double d = 0.1; 
    printf("tengo este float: %f y este double: %f \n --casteo--", f,d); 
    int f1 = (int) f;
-   int d1 = (int) d1;
+   int d1 = (int) d;
    printf("tengo este float: %i y este double: %i", f1,d1); 
 
     return 0;
