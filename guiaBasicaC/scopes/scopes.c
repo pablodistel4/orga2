1ded4beae836e5e075e177055342e1843f1bff51
Pablo Distel <pablodistel4@gmail.com>
2026-04-17 18:37:45 -0300

    organizo las guias en carpetas

---
+++ guiaBasicaC/scopes/scopes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define FELIZ 0
+#define TRISTE 1
+
+int estado = TRISTE; // static duration. File scope
+
+void alcoholizar();
+void print_estado();
+
+int main(){
+    print_estado();
+    alcoholizar();
+    print_estado();
+    alcoholizar();alcoholizar();alcoholizar();
+    print_estado(); // que imprime?
+}
+void alcoholizar(){
+static int cantidad = 0; // static duration. block scope
+cantidad++;
+printf("cant de tragos:%i\n",cantidad);
+if(cantidad < 3){
+estado = FELIZ;
+}else{
+estado = TRISTE;
+}
+}
+void print_estado(){
+printf("Estoy %s\n", estado == FELIZ ? "feliz" : "triste");
+}
